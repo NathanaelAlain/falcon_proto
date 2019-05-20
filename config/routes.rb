514d215a029ba867cb7ea7fad074e64b11716988
Maarten Esser <maartenjr.esser@gmail.com>
2019-05-20 17:46:07 +0200

    added the redirect after login and created a controller for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+  root to: "devise/sessions#new"
+  end
+
+  get '/dashboard', to: 'pages#dashboard', as: :dashboard
 end
