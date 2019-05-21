a140e034d2998eb481be4f82b71605f41bc61164
Maarten Esser <maartenjr.esser@gmail.com>
2019-05-21 15:36:45 +0200

    added menu items fore the user so he can log out, and change his data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :seller, :address, :photo_url])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :seller, :address, :photo_ur])
   end
 
   include Pundit
