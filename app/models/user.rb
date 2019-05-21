5c9ac83e403f346228735c8c7adb69fb569bc6a6
Maarten Esser <maartenjr.esser@gmail.com>
2019-05-21 16:19:03 +0200

    added the photo upload of cloudinary so you can upload an avatar image

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :parts
+  mount_uploader :photo, PhotoUploader
 end
