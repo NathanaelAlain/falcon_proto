bf3dd3a63809523b1d14844b9cd1ec52188e5abe
Romeo Saint-Albin <r.stalbin@gmail.com>
2019-05-21 12:44:56 +0200

    Navbar with falcon icon and avatar

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,10 @@ ActiveRecord::Schema.define(version: 2019_05_20_173246) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "address"
+    t.string "first_n"
+    t.string "last_n"
+    t.string "photo_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
