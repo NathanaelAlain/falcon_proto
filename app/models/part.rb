2e4eea199bd49cfb2314ad797f7e7d9ea4e1b041
Nathanael Alain <nathanaelalain@yahoo.fr>
2019-05-21 11:45:14 +0200

    add part dependent destroy  and validation

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,3 +1,7 @@
 class Part < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
+
+  validates :user_id, presence: true
+  validattes :name, presence: true
+  validattes :price, presence: true
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :part
+
+  validates :user_id, presence: true
+  validates_uniqueness_of :user_id, scope: :part_id
 end
