dac779eac48bdbb5839491e48bc32820d9c01a67
Romeo Saint-Albin <r.stalbin@gmail.com>
2019-05-22 15:21:14 +0200

    Add has_many to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :parts
+  has_many :trades
 
   mount_uploader :photo, PhotoUploader
-
 end
