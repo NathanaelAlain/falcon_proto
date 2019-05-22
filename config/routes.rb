b11b90a057f41d8d4c089b5aa3931168746fbe72
Gaël Tims <47397918+gaeltims@users.noreply.github.com>
2019-05-22 14:36:42 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,7 @@ Rails.application.routes.draw do
   #Declared two actions of pages
   get '/dashboard', to: 'pages#dashboard', as: :dashboard
   get '/profile', to: 'pages#profile'
-
-  get 'parts/:id/edit/sold', to: 'parts#form_sold', as: :sold
-
+  
   resources :parts do
     resources :transactions, only: [:new, :create]
   end
