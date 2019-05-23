9a399ba1ad93d6ec0d98f6783c5867a7b0d35d3f
Gael Tims <gaeltims@gmail.com>
2019-05-23 20:03:20 +0200

    new integration of navbar - deleted old top navbar & integrated it into sidenavbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,14 @@
 import "bootstrap";
+import { openNav, closeNav } from "./sidenav";
+
+let sidenav = document.querySelector(".openbtn");
+  sidenav.addEventListener('click', () => {
+    event.preventDefault();
+    openNav();
+  });
+
+let sidenav_close = document.querySelector(".closebtn");
+  sidenav_close.addEventListener('click', () => {
+    event.preventDefault();
+    closeNav();
+  });
